1a47017d0a1d3844d16caa8aa30206c8c72f1d3c
Matthew Denwood <md@sund.ku.dk>
2020-04-08 08:00:45 +0200

    Update fetch_drat.R

--- rsc/fetch_drat.R
+++ rsc/fetch_drat.R
@@ -2,6 +2,8 @@
 
 # Functions taken from the R pacakge:
 
+.wrapfun <- function(){
+
 check_curl <- 
 function (feedback) 
 {
@@ -18,6 +20,68 @@ function (feedback)
         }
         return(FALSE)
     }
+}
+
+safe_unzip <- 
+function (zipfile, exdir = gsub("\\.zip$", "", zipfile), files = NULL, 
+    silent = TRUE) 
+{
+    if (!silent) 
+        cat("Unzipping the file/folder...\n")
+    prompted <- FALSE
+    ss <- try({
+        zip::unzip(zipfile, files = files, exdir = exdir)
+    })
+    if (inherits(ss, "try-error")) {
+        unlink(exdir, recursive = TRUE)
+        if (Sys.info()["sysname"] == "Darwin") {
+            cat("Unzipping the file failed with zip::unzip - trying again with system utilities...\n")
+            Sys.sleep(0.5)
+            ss <- try({
+                system(str_c("open \"", zipfile, "\""))
+            })
+            readline(prompt = "Hit any key to continue")
+            prompted <- TRUE
+        }
+    }
+    if (inherits(ss, "try-error")) {
+        unlink(exdir, recursive = TRUE)
+        if (!interactive()) 
+            stop("File could not be unzipped automatically")
+        cat("An error occured while using zip::unzip from within R - please uncompress the folder manually\nOpening folder...")
+        Sys.sleep(1)
+        system(str_c("open \"", gsub("/[^/]*$", "", zipfile), 
+            "\""))
+        system(str_c("open \"", zipfile, "\""))
+        readline(prompt = "Hit any key to continue")
+        prompted <- TRUE
+    }
+    if (!file.exists(exdir)) {
+        file.copy(zipfile, getwd())
+        stop("Attempt to unzip failed - the zip file has been copied to your working directory", 
+            call. = FALSE)
+    }
+    fld <- strsplit(zipfile, "/", fixed = TRUE)[[1]]
+    fld <- gsub(".zip", "", fld[length(fld)], fixed = TRUE)
+    if (file.exists(file.path(exdir, fld)) && file.info(file.path(exdir, 
+        fld))[1, "isdir"]) {
+        exdir <- file.path(exdir, fld)
+    }
+    if (!prompted) {
+        dly <- suppressWarnings(as.numeric(Sys.getenv("SAFE_ZIP_DELAY")))
+        if (Sys.getenv("SAFE_ZIP_DELAY") != "" && is.na(dly)) {
+            readline(prompt = "Hit any key to continue")
+        }
+        else if (!is.na(dly)) {
+            Sys.sleep(dly)
+        }
+        else {
+            Sys.sleep(0.5)
+        }
+    }
+    if (!silent) 
+        cat("Done\n")
+    return(exdir)
 }
 
 fetch_drat_ll <- 
@@ -62,17 +126,21 @@ function (server = "", userpwd = "", feedback, silent = FALSE)
     if (inherits(ss, "try-error")) {
         if (!silent) 
             cat("\n")
+        unlink(tmp, recursive = TRUE)
         stop("An error occured while fetching the drat repository - check the message above for clues", 
             call. = FALSE)
     }
-    zip::unzip(file.path(tmp, "drat.zip"), exdir = tmp)
+    Sys.setenv(TEMP_SFTP_SERVER = server)
+    Sys.setenv(TEMP_SFTP_USERPWD = userpwd)
+    safe_unzip(file.path(tmp, "drat.zip"), exdir = tmp)
+    tr <- paste0("file:", file.path(tmp, tempfile = "drat"))
     r <- getOption("repos")
-    if ("covid19drat" %in% names(r)) {
-        r["covid19drat"] <- paste0("file:", file.path(tmp, tempfile = "drat"))
+    if ("tmpdrat" %in% names(r)) {
+        unlink(r["tmpdrat"], recursive = TRUE)
+        r["tmpdrat"] <- tr
     }
     else {
-        r <- c(r, covid19drat = paste0("file:", file.path(tmp, 
-            tempfile = "drat")))
+        r <- c(r, tmpdrat = tr)
     }
     options(repos = r)
     if (!silent) 
@@ -93,3 +161,6 @@ if(!suppressPackageStartupMessages(require('curl', quietly=TRUE))) stop('The cur
 if(!suppressPackageStartupMessages(require('zip', quietly=TRUE))) stop('The zip package could not be loaded - please make sure that both the curl and zip packages are installed and try again')
 
 fetch_drat_ll(server='', userpwd='', feedback='Re-source this online script', silent=FALSE)
+}
+
+.wrapfun()
