656a6bd72c48257813af45546225fb9ea22ae9e8
Matt Denwood <md@sund.ku.dk>
2023-03-17 08:11:11 +0100

    Tweak topic 2

--- teaching/voc_data_science/topic2/solution_topic_2.R
+++ teaching/voc_data_science/topic2/solution_topic_2.R
@@ -7,18 +7,36 @@ library("readxl")
 
 
 ## Read the sheets available:
-
 filename <- "solution_topic_1.xlsx"
 sheets <- excel_sheets(filename)
+sheets
+
+
+## Read gait score lookup table:
+gait_lookup <- read_excel(filename, sheet="gait_lookup")
+str(gait_lookup)
+
+gait_lookup$GaitScore <- factor(gait_lookup$GaitScore)
+gait_levels <- levels(gait_lookup$GaitScore)
+str(gait_lookup)
+
+
+## Read breed lookup table:
+breed_lookup <- read_excel(filename, sheet="breed_lookup")
+str(breed_lookup)
+
+breed_lookup$Breed <- factor(breed_lookup$Breed)
+breed_levels <- levels(breed_lookup$Breed)
+str(breed_lookup)
+summary(breed_lookup)
 
 
 ## Read and check the farm data:
-
 farm_data <- read_excel(filename, sheet="farm_data")
 str(farm_data)
 
-farm_data$HerdType <- factor(farm_data$HerdType)
-farm_data$MilkSystem <- factor(farm_data$MilkSystem)
+farm_data$HerdType <- factor(farm_data$HerdType, levels=c("Conventional","Organic"))
+farm_data$MilkSystem <- factor(farm_data$MilkSystem, levels=c("Rotary parlour","Herringbone parlour","Robotic"))
 farm_data$VisitDate <- as.Date(farm_data$VisitDate)
 
 str(farm_data)
@@ -26,29 +44,26 @@ summary(farm_data)
 
 
 ## Read and check the visit data:
-
 visit_data <- read_excel(filename, sheet="visit_data")
 str(visit_data)
 
-visit_data$GaitScore <- factor(visit_data$GaitScore, levels=c(0,1,2,3,4))
+visit_data$GaitScore <- factor(visit_data$GaitScore, levels=gait_levels)
 str(visit_data)
 summary(visit_data)
 
 
 ## Read and check the registry_animal data:
-
 registry_animal <- read_excel(filename, sheet="registry_animal")
 str(registry_animal)
 
 registry_animal$DOB <- as.Date(registry_animal$DOB)
-registry_animal$Breed <- factor(registry_animal$Breed)
+registry_animal$Breed <- factor(registry_animal$Breed, levels=breed_levels)
 
 str(registry_animal)
 summary(registry_animal)
 
 
 ## Read and check the registry_calving data:
-
 registry_calving <- read_excel(filename, sheet="registry_calving")
 str(registry_calving)
 
@@ -58,7 +73,6 @@ summary(registry_calving)
 
 
 ## Read and check the registry_milk data:
-
 registry_milk <- read_excel(filename, sheet="registry_milk")
 str(registry_milk)
 
@@ -66,23 +80,6 @@ registry_milk$MilkDate <- as.Date(registry_milk$MilkDate)
 str(registry_milk)
 summary(registry_milk)
 
-
-## Read and check the lookup tables:
-
-breed_lookup <- read_excel(filename, sheet="breed_lookup")
-str(breed_lookup)
-
-breed_lookup$Breed <- factor(breed_lookup$Breed)
-str(breed_lookup)
-summary(breed_lookup)
-
-
-gait_lookup <- read_excel(filename, sheet="gait_lookup")
-str(gait_lookup)
-
-gait_lookup$GaitScore <- factor(gait_lookup$GaitScore, levels=c(0,1,2,3,4))
-str(gait_lookup)
-summary(gait_lookup)
 
 
 ## Finished!
